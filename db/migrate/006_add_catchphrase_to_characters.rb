367b26d26692cd9694f6f8aa688514e0122b04dd
Gabriel Miranda <gabe.enrique.miranda@gmail.com>
2021-10-02 15:07:06 -0700

    commit tree 726b24cad9168a2e1d79f50d63da46587f12e640

--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,5 +1,6 @@
 class AddCatchphraseToCharacters < ActiveRecord::Migration[6.1]
   def change
-    # Your code here
+    # Your code here (table_name, column_name, type, **options)
+    add_column :characters, :catchphrase, :string
   end
 end
